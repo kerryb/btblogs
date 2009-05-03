2992beb70f947b5ac77fdbd339ea72dffb32dd9c
Kerry Buckley <kerryjbuckley@gmail.com>
2009-05-03 20:42:23 +0100

    Fixed column name in blog model spec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -5,7 +5,7 @@ describe Blog do
     Blog.new.should be_a_kind_of(ActiveRecord::Base)
   end
 
-  it { should have_db_column(:title).of_type(:string) }
+  it { should have_db_column(:owner_name).of_type(:string) }
   it { should have_db_column(:owner_email).of_type(:string) }
   it { should have_db_column(:html_uri).of_type(:string) }
   it { should have_db_column(:feed_uri).of_type(:string) }
