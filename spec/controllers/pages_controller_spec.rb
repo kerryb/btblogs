9b62fa51670d1f05fcd787feeb06f19f96a454e6
Kerry Buckley <kerryjbuckley@gmail.com>
2009-05-11 08:55:18 +0100

    Remove templator-specific suffix from render

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PagesController do
   describe '#home' do
-    it 'should render home.html.erb' do
+    it 'should render home' do
       get :home
-      response.should render_template('home.html.erb')
+      response.should render_template('home')
     end
   end
 end
