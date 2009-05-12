f571beca5be2b4633118ac00d99f8fac001e437c
Kerry Buckley <kerryjbuckley@gmail.com>
2009-05-12 22:34:49 +0100

    Converted layout to HAML

---
+++ spec/views/layouts/default.html.haml_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe 'The default layout' do
+  def do_render
+    render :text => '', :layout => true
+  end
+
+  it 'should render links to all blogs' do
+    blog_1 = Factory :blog, :owner_name => 'Amy Acker', :html_uri => 'foo'
+    blog_2 = Factory :blog, :owner_name => 'Bill Bailey', :html_uri => 'bar'
+    assigns[:blogs] = [blog_1, blog_2]
+    do_render
+    response.should have_tag('div#blogs') do
+      with_tag "a[href='foo']", 'Amy Acker'
+      with_tag "a[href='bar']", 'Bill Bailey'
+    end
+  end
+
+  describe 'when there is a flash notice' do
+    it 'should render the notice' do
+      flash[:notice] = 'Hello'
+      do_render
+      response.should have_tag('div#notice', 'Hello')
+    end
+  end
+
+  describe 'when there is not a flash notice' do
+    it 'should not render a notice div' do
+      do_render
+      response.should_not have_tag('div#notice')
+    end
+  end
+end
