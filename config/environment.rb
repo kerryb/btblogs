10deea1f9cca15dfb0bbfd23d16a656022d51fde
Kerry Buckley <kerryjbuckley@gmail.com>
2009-07-24 20:47:31 +0100

    Switched to geminstaller

--- config/environment.rb
+++ config/environment.rb
@@ -25,9 +25,6 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'rack', :version => '>=1.0'
-  config.gem 'haml-edge', :version => '2.1.3', :lib => 'haml'
-  config.gem 'chriseppstein-compass', :version => '0.6.6', :lib => 'compass', :source => 'gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
